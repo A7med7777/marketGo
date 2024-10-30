a950ba10ab4485794a5770d39fd3205882d3b907
A7med7777 <ahmedhessian56@gmail.com>
2024-10-30 11:44:08 +0300

    Handle CORS issues using django-cors-headers

--- marketgo/settings.py
+++ marketgo/settings.py
@@ -32,18 +32,20 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'corsheaders', 
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users', 
     'rest_framework', 
     'rest_framework.authtoken', 
+    'users', 
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware', 
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -51,6 +53,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",  # replace with your frontend origin
 ]
 
 ROOT_URLCONF = 'marketgo.urls'
